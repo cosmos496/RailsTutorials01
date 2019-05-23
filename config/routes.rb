6b0a09da0687893326503c9b19d3bb60c1a9077a
Izumi Yusuke <xizumi.yusuke@contract.isid.co.jp>
2019-05-23 11:11:11 +0900

    20190523_myGit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'main_ctrl#main'
+  # root :to => 'main_ctrl#main'
   get 'login' => 'main_ctrl#login'
     # ↑
     # "https://www.javadrive.jp/rails/ini/index4.html" では
