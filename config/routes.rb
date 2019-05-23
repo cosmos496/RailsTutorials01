33c023507db296bcecf76b9cd6b491ee70fa9747
Izumi Yusuke <xizumi.yusuke@contract.isid.co.jp>
2019-05-23 11:12:27 +0900

    20190523_myGit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root :to => 'main_ctrl#main'
+  root :to => 'main_ctrl#main'
   get 'main' => 'main_ctrl#main'
   get 'login' => 'main_ctrl#login'
     # ↑
